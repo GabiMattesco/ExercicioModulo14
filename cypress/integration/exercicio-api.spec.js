ecb7882f5a195c9e47de9b643b5c876ec30eed65
Gabriela Mattesco <gabimattesco@gmail.com>
2022-05-03 19:27:11 -0300

    alteracao

--- cypress/integration/exercicio-api.spec.js
+++ cypress/integration/exercicio-api.spec.js
@@ -1,100 +1,100 @@
 /// <reference types="cypress" />
 import contrato from '../contracts/usuarios.contract'
+var faker = require('faker');
+
 
 describe('Testes da Funcionalidade Usuários', () => {
 
-     it('Deve validar contrato de usuários', () => {
-          cy.request('usuario').then(response => {
-               return contrato.validateAsync(response.body)
-          })
-     });
+    it('Deve validar contrato de usuários', () => {
+        cy.request('usuarios').then(response => {
+            return contrato.validateAsync(response.body)
+            url: 'usuarios'
+        })
+    });
 
-     it('Deve listar usuários cadastrados', () => {
-          cy.request({
-               method: 'GET',
-               url: 'usuarios',
+    it('Deve listar usuários cadastrados', () => {
+        cy.request({
+            method: 'GET',
+            url: 'usuarios',
 
-          }).then((response) => {
-               expect(response.status).to.equal(200)
-          })
-     });
+        }).then((response) => {
+            expect(response.status).to.equal(200)
+        })
+    });
 
-     it('Deve cadastrar um usuário com sucesso', () => {
-          let usuario = `Usuario EBAC ${Math.floor(Math.random() * 1000000)}`
-          let email = `${usuario}@qa.com`
-          cy.request({
-               method: 'POST',
-               url: 'usuarios',
-               body: {
-                    "nome": usuario,
-                    "email": email,
-                    "password": "teste#$%",
-                    "administrador": "true"
-               }
-          }).then((response) => {
-               expect(response.body.message).to.equal('Cadastro realizado com sucesso')
-               expect(response.status).to.equal(201)
-          });
+    it('Deve cadastrar um usuário com sucesso', () => {
+        let usuario = `Usuario EBAC ${Math.floor(Math.random() * 1000000)}`
+        cy.request({
+            method: 'POST',
+            url: 'usuarios',
+            body: {
+                "nome": usuario,
+                "email": faker.internet.email(),
+                "password": "teste#$%",
+                "administrador": "true"
+            }
+        }).then((response) => {
+            expect(response.body.message).to.equal('Cadastro realizado com sucesso')
+            expect(response.status).to.equal(201)
+        });
 
-     });
+    });
 
-     it('Deve validar um usuário com email inválido', () => {
-          cy.request({
-               method: 'GET',
-               url: 'usuarios',
-               body: {
-                    "nome": "Daniela Silva",
+    it('Deve validar um usuário com email inválido', () => {
+        cy.request({
+            method: 'POST',
+            url: 'usuarios',
+            body: {
+                "nome": "Bernardo Gomes",
+                "email": "teste@qa.com",
+                "password": "xxxx",
+                "administrador": "true"                
+            }, failOnStatusCode: false
+
+        }).then((response) => {
+            expect(response.status).to.equal(400)
+            expect(response.body.message).to.equal('Este email já está sendo usado')
+        });
+    });
+
+    it('Deve editar um usuário previamente cadastrado', () => {
+        cy.request('usuarios').then(response => {
+            let id = response.body.usuarios[0]._id
+            cy.request({
+                method: 'PUT',
+                url: `usuarios/${id}`,
+                body: {
+                    "nome": "Fulano da Silva Gomes",
                     "email": "fulano@qa.com.br",
-                    "password": "!!!XXX",
-                    "administrador": "true",
-                    "_id": "eqQuBFPg7XNFRRha"
-               }
+                    "password": "@@@!!!F",
+                    "administrador": "true"
+                }
 
-          }).then((response) => {
-               expect(response.status).to.equal(200)
-               expect(response.body.message).to.equal('Este email já está sendo usado')
-          });
-     });
+            }).then((response) => {
+                expect(response.body.message).to.equal('Registro alterado com sucesso')
+                expect(response.status).to.equal(200)
+            });
+        })
 
-     it('Deve editar um usuário previamente cadastrado', () => {
-          cy.request('usuarios').then(response => {
-               let id = response.body.usuarios[0]._id
-               cy.request({
-                    method: 'PUT',
-                    url: `usuarios/${id}`,
-                    body: {
-                         "nome": "Fulano da Silva Gomes",
-                         "email": "fulano@qa.com.br",
-                         "password": "@@@!!!F",
-                         "administrador": "true"
-                    }
+    });
+    it('Deve deletar um usuário previamente cadastrado', () => {
+        cy.request('usuarios').then(response => {
+            let id = response.body.usuarios[0]._id
+            cy.request({
+                method: 'DELETE',
+                url: `usuarios/${id}`,
+                body: {
+                    "nome": "Fulano da Silva Gomes",
+                    "email": "fulano@qa.com.br",
+                    "password": "@@@!!!F",
+                    "administrador": "true"
+                }
 
-               }).then((response) => {
-                    expect(response.body.message).to.equal('Registro alterado com sucesso')
-                    expect(response.status).to.equal(200)
-               });
-          })
-
-     });
-     it('Deve deletar um usuário previamente cadastrado', () => {
-          cy.request('usuarios').then(response => {
-               let id = response.body.usuarios[0]._id
-               cy.request({
-                    method: 'DELETE',
-                    url: `usuarios/${id}`,
-                    body: {
-                         "nome": "Fulano da Silva Gomes",
-                         "email": "fulano@qa.com.br",
-                         "password": "@@@!!!F",
-                         "administrador": "true"
-                    }
-
-               }).then((response) => {
-                    expect(response.body.message).to.equal('Registro excluído com sucesso')
-                    expect(response.status).to.equal(200)
-               });
-          })
-     });
-
+            }).then((response) => {
+                expect(response.body.message).to.equal('Registro excluído com sucesso')
+                expect(response.status).to.equal(200)
+            });
+        })
+    });
 
 });
